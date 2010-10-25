7a25108c346a4a91da852636e3047a652edf64b0
Xiao G. Wu <xiaogwu@gmail.com>
2010-10-25 12:50:58 -0700

    Populated the elements of a NSMutableArray with NSNumber value of (i *3) and printing out the values

--- lottery.m
+++ lottery.m
@@ -3,8 +3,25 @@
 int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
-    // insert code here...
-    NSLog(@"Hello, World!");
+	// Declare NSMutableArray and init
+	NSMutableArray *array;
+	array = [[NSMutableArray alloc] init];
+	
+	// Loop through array (10 times) setting each element of array to a NSNumber value of (i * 3)
+	int i;
+	for (i = 0; i < 10; i++) {
+		NSNumber *newNumber;
+		newNumber = [[NSNumber alloc] initWithInt: (i * 3)];
+		[array addObject: newNumber];
+	}
+	
+	// Loop through array (10 times) printing each element of array to NSLog
+	for (i = 0; i < 10; i++) {
+		NSNumber *numberToPrint;
+		numberToPrint = [array objectAtIndex: i];
+		NSLog(@"Number at index %d is %@", i, numberToPrint);
+	}
+	
     [pool drain];
     return 0;
 }
